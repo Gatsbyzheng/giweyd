8c957539243fc763945c9f8779d243d7b8543f65
gatsby <gatsbyzheng@outlook.com>
2021-07-02 17:04:49 +0800

    feat(接口文档): 增加接口文档测试代码

--- src/main/java/ink/gatsby/giweyd/controller/TestController.java
+++ src/main/java/ink/gatsby/giweyd/controller/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Test {
+public class TestController {
 
     @GetMapping("success")
     public CommonResponse<String> success() {
---
+++ src/test/java/ink/gatsby/giweyd/TestControllerTests.java
@@ -0,0 +1,28 @@
+package ink.gatsby.giweyd;
+
+import ink.gatsby.giweyd.controller.TestController;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(TestController.class)
+@AutoConfigureRestDocs
+public class TestControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void success() throws Exception {
+        mockMvc.perform(get("/success"))
+                .andExpect(status().isOk())
+                .andDo(document("success"));
+    }
+}
